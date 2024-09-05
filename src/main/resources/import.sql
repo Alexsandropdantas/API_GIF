2ca6695fe164a457dcf411e2cf40b1d8fa57a0f0
Alexsandro Paulino Dantas <alexsandropdantas@gmail.com>
2024-09-05 01:48:24 -0300

    Adicionando dados no banco de dados

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO solicitacao (servico_solicitacao, nome_solicitante, email_solicitante, cnpj, migracao_prodep_proind, natureza_projeto, estabelecimento, qtd_empregos, valor_investimento, tipo_anexo, arquivo_pdf)
+VALUES ('Serviço A', 'João Silva', 'joao.silva@email.com', '12345678000195', 'Sim', 'Comércio', 'Estabelecimento X', 10, 100000.00, 'PDF', 'arquivoA.pdf');
+
+INSERT INTO solicitacao (servico_solicitacao, nome_solicitante, email_solicitante, cnpj, migracao_prodep_proind, natureza_projeto, estabelecimento, qtd_empregos, valor_investimento, tipo_anexo, arquivo_pdf)
+VALUES ('Serviço B', 'Maria Oliveira', 'maria.oliveira@email.com', '98765432000189', 'Não', 'Indústria', 'Estabelecimento Y', 25, 250000.00, 'PDF', 'arquivoB.pdf');
